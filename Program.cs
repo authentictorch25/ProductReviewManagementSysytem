6a9cbc9071390fdfb701df0048b36dc704db78a0
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-15 18:11:30 +0530

    [Akash] Added program to add product in the system

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductReviewSystem
 {
@@ -6,7 +7,27 @@ namespace ProductReviewSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to product review management!\n");
+            //UC 1
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=101,Rating=5,Review="Excellent",IsLike=true },
+                new ProductReview(){ProductID=3,UserID=106,Rating=5,Review="Excellent",IsLike=true },
+                new ProductReview(){ProductID=2,UserID=102,Rating=4,Review="Good     ",IsLike=true },
+                new ProductReview(){ProductID=4,UserID=107,Rating=4,Review="Good     ",IsLike=true },
+                new ProductReview(){ProductID=3,UserID=103,Rating=3,Review="Average  ",IsLike=true },
+                new ProductReview(){ProductID=2,UserID=108,Rating=3,Review="Average  ",IsLike=true },
+                new ProductReview(){ProductID=4,UserID=104,Rating=2,Review="Satisfactory",IsLike=false },
+                new ProductReview(){ProductID=1,UserID=109,Rating=2,Review="Satisfactory",IsLike=false },
+                new ProductReview(){ProductID=5,UserID=105,Rating=1,Review="Poor     ",IsLike=false },
+                new ProductReview(){ProductID=5,UserID=110,Rating=1,Review="Poor     ",IsLike=false }
+            };
+            
+            foreach (var v in productReviewList)
+            {
+                Console.WriteLine($"ProductID:{v.ProductID}\tUserID:{v.UserID}\tRating:{v.Rating}\tReview:{v.Review}\tIsLike:{v.IsLike}");
+            }
         }
+    }
     }
 }
